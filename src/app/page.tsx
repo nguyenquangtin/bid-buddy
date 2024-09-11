fb5d55b2add28bcbc2f6ca1cbaa73d4699e82284
Tony Nguyen <nguyenquangtin@gmail.com>
2024-09-11 14:07:02 +0700

    Rename the schema and rebuild the db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import { bids as bidsSchema } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
+import SignIn from "@/components/sign-in";
+import SignOut from "@/components/sign-out";
+import { auth } from "@/auth";
 
 export default async function Homepage() {
   const bids = await database.query.bids.findMany(); // get all
 
+  const session = await auth();
+
   return (
     <main className="">
+      {session ? <SignOut /> : <SignIn />}
+      {session?.user?.name}
       <form
         className="contrainer mx-auto"
         action={async (formData: FormData) => {
---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { database } from "@/db/database";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { users, accounts, sessions, verificationTokens } from "./db/schema";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(database, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  providers: [Google],
+});
