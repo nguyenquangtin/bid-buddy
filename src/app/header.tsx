40e149b51cbacb8b712c952f6b2d463d3d87a38d
Tony Nguyen <nguyenquangtin@gmail.com>
2024-09-12 16:16:42 +0700

    add the Header for the site

---
+++ src/app/header.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
+import SignOut from "@/components/sign-out";
+import Image from "next/image"
+import Link from "next/link";
+
+export async function Header() {
+  const session = await auth();
+
+  return (
+    <div className="bg-gray-50 py-4">
+      <div className="container flex justify-between mx-auto">
+
+        <Link href="/" className="hover:underline flex items-center gap-1">
+          <Image src="/logo.svg" width="50" height="50" alt="logo" />
+          EcomdyBuddy.com
+        </Link>
+
+        <div className="flex">
+          {session ? <SignOut /> : <SignIn />}
+        </div>
+
+      </div>
+    </div>
+  )
+}
